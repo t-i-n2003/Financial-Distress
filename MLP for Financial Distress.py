af0255ad5c40b63d065e7205150e50e92c7377b5
t-i-n2003 <91058119+t-i-n2003@users.noreply.github.com>
2024-04-25 08:22:09 +0700

    Add files via upload

---
+++ MLP for Financial Distress.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("D:\\Financial Distress\\Financial Distress.csv")
+
+condition = df['Financial Distress'] > -0.50
+
+df['Financial Distress'] = df['Financial Distress'].apply(lambda x: 1 if x > -0.50 else 0)
+
+X = df[['Company', 'Time', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7',
+        'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17',
+        'x18', 'x19', 'x20', 'x21', 'x22', 'x23', 'x24', 'x25', 'x26', 'x27',
+        'x28', 'x29', 'x30', 'x31', 'x32', 'x33', 'x34', 'x35', 'x36', 'x37',
+        'x38', 'x39', 'x40', 'x41', 'x42', 'x43', 'x44', 'x45', 'x46', 'x47',
+        'x48', 'x49', 'x50', 'x51', 'x52', 'x53', 'x54', 'x55', 'x56', 'x57',
+        'x58', 'x59', 'x60', 'x61', 'x62', 'x63', 'x64', 'x65', 'x66', 'x67',
+        'x68', 'x69', 'x70', 'x71', 'x72', 'x73', 'x74', 'x75', 'x76', 'x77',
+        'x78', 'x79', 'x80', 'x81', 'x82', 'x83']]
+
+y = df['Financial Distress']
+
+# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 40)
+
+clf = MLPClassifier(
+                        hidden_layer_sizes=(5, 4, 5),
+                        random_state=4,
+                        verbose=True,
+                        activation='tanh',
+                        solver='sgd',
+                        alpha=0.0001,
+                        learning_rate_init=1
+                    )
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print(accuracy_score(y_test, y_pred))
